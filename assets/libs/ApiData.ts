0ee58a565b7631057719d3ef85c4524e7a2eb6ca
adu <adu@newpharma.ro>
2021-06-10 14:58:54 +0300

    Ts project

--- assets/libs/ApiData.ts
+++ assets/libs/ApiData.ts
@@ -1,14 +1,10 @@
 import {apiData} from "../data/mockData";
-import {UserInterface} from "./UserInterface";
 
 export class ApiData {
+
     public static getUsers()
     {
         return apiData;
     }
 
-    public static getUserById(id:number)
-    {
-
-    }
 }
